2db27dfa58d55f065fbf62565f8b5ae4ee2a6798
JiriCvejn <jiri.cvejn94@gmail.com>
2025-07-31 11:40:17 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const sequelize = require('./models/index');
+require('./models/User');
+require('./models/Task');
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+sequelize.sync().then(() => console.log('Databáze připravena'));
+
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/tasks', require('./routes/tasks'));
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server běží...");
+});
